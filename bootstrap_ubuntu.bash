79eb85644802944bd142df9516a7186e5f1deeff
rita <paradise0917@gmail.com>
2023-12-17 01:53:09 +0800

    feat: add ubuntu bootstrap script

--- bootstrap_ubuntu.bash
+++ bootstrap_ubuntu.bash
@@ -2,19 +2,11 @@
 
 set -e
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> ~/.zprofile
-eval "$(/usr/local/bin/brew shellenv)"
-
-brew install --cask slack notion 
-brew install --cask visual-studio-code docker google-chrome firefox iterm2 figma
-brew install gh
-brew install --cask spotify
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install --lts
 
+sudo apt install zsh
 export RUNZSH=no
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
